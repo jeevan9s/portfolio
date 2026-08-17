39f193c876045af4271f9c400e58382c672c1ff6
Jeevan Sanchez <jeevansanchez42@gmail.com>
2026-08-17 10:26:01 -0400

    update favicon

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-[#282727]">
-      <div className="bg-white flex-2 w-32 h-32"></div>
     </div>
   );
 }
